f8f0c57415d3adec6b6eb718b8d6cc3d46ca939f
Johannes Sundqvist <johannes@israproductions.com>
2016-10-02 10:23:32 +0200

    Initial commit

---
+++ app/src/main/java/nu/isra/kids/MainActivity.java
@@ -0,0 +1,14 @@
+package nu.isra.kids;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
